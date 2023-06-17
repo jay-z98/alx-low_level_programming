3d541a845e52d3e0aac84f1115e403a2318a016f
jay-z98 <emmanuel.owusu103@icloud.com>
2023-06-17 14:05:33 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-/*
- * main - A program that prints a put function
- * Return: 0 (Success)
- *\
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\");
-Return: (0);
+       	puts("\"Programming is like building a multilingual puzzle\"");
+	        return (0);
 }
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * main - The use of printf command
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+Return:(0);
+}
