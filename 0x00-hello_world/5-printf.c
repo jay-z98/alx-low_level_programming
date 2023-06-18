5cb39b90fbe7ffefad533247b54a658f29eb99ba
jay-z98 <emmanuel.owusu103@icloud.com>
2023-06-18 05:51:15 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /*
- * main - The use of printf command
+ * main - The program that uses printf function
+ *
+ * Description: 'A program that prints grammar'
  */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 Return:(0);
 }
