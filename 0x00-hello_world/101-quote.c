c3567874ce6d485b7201061c99a049c15addd32a
jay-z98 <emmanuel.owusu103@icloud.com>
2023-06-18 11:16:50 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+const char *message =
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+fputs(message, stderr);
+
+return (1);
+}
