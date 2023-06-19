62d67882fe470214523ccd92853eafe406fd1407
jay-z98 <emmanuel.owusu103@icloud.com>
2023-06-19 09:55:34 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,38 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
-/**
- * main - Entry point of the program
- *
- * Determines if a generated number is possitive,
- * negative, or zero.
- *
- * Return: Always 0 (uccess)
- */
-int main(void)
-{
-int n;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else
-{
-printf("%d is negative\n", n);
-}
-return (0);
-}
-
-
 #include <stdio.h>
 
 /**
