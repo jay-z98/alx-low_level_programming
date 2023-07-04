00ffbfc5117b7fdc107b7333bec8f1198049dfd2
jay-z98 <emmanuel.owusu103@icloud.com>
2023-07-04 10:29:13 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - generates keygen.
- * Return: 0 Always.
- */
+#include <stdint.h>
+
 int main(void)
 {
 int r = 0, c = 0;
 time_t t;
 
 srand((unsigned int) time(&t));
+
+char keygen[2773];  // Increased size to accommodate the final character and null terminator
+int index = 0;
+
 while (c < 2772)
 {
 r = rand() % 128;
 if ((c + r) > 2772)
 break;
 c = c + r;
-printf("%c", r);
+keygen[index] = (char) r;
+index++;
 }
-printf("%c\n", (2772 - c));
+keygen[index] = (char) (2772 - c);
+keygen[index + 1] = '\0';  // Add null terminator at the end of the string
+
+printf("%s\n", keygen);
+
 return (0);
 }
