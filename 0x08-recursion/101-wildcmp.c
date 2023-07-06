f94a9311ce7711d5cc1e7029daed825b41ca3a26
jay-z98 <emmanuel.owusu103@icloud.com>
2023-07-05 18:12:25 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**
+/*
  * wildcmp - compares two strings and returns 1 if the strings
  *!* can be considered identical, otherwise return 0.
  * @s1: string to compare to
@@ -21,6 +21,7 @@ if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 return (0);
 if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
 return (1);
+}
 
 return (0);
 }
